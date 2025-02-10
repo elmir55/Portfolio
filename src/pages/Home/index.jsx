e956eea459cb4b7bc19c58cdd0ab1206bf81405c
elmir55 <elmira.050405@gmail.com>
2025-02-10 16:25:59 +0600

    added home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Block1 from '../../components/block1'
+import Block1 from '../../components/Block1'
 import Block2 from '../../components/Block2'
 import { Block4 } from '../../components/Block4'
 import { Block3 } from '../../components/Block3'
